5ed204bea0e2cc192cddfb6361fa725627d59c18
leticiamontilla <leticia.m.code@gmail.com>
2026-01-20 12:21:09 +0100

    Modificado el fichero Main.java en la segunda rama

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Esto es un fichero .java");
+        System.out.println("Esto es el fichero .java de la segunda rama");
     }
 }
